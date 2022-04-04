e1ea6e4439697237d4411bab5d54a1157e81f297
Mario S. Pinto Miranda <mario.pinto.miranda@gmail.com>
2022-04-04 17:22:26 +0100

    RED : RomanNumeralEncoder.encode_the_basics_symbols

--- src/test/java/RomanEncoderShould.java
+++ src/test/java/RomanEncoderShould.java
@@ -1,12 +1,32 @@
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+
+class RomanNumeralEncoder{
+
+    public String encode(int i) {
+    }
+}
+
 
 public class RomanEncoderShould {
 
+    RomanNumeralEncoder romanNumeralEncoder;
+    @BeforeEach
+    void setUp(){
+        romanNumeralEncoder = new RomanNumeralEncoder();
+    }
+
     @Test
     void pass_happy_test(){
-        
+        assertEquals(1,1);
     }
 
 
+    @Test
+    void encode_the_basics_symbols(){
+        assertEquals("I", romanNumeralEncoder.encode(1));
+    }
 
 }
